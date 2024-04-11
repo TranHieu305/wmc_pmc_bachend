acd5672a843db098d28a7d2ab6955343ff27d669
TranHieu305 <trunghieu2000.tdn@gmail.com>
2024-04-11 18:57:43 +0700

    Add CrossOrigin to controller

--- src/main/java/com/wms/wms/controller/WarehouseController.java
+++ src/main/java/com/wms/wms/controller/WarehouseController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "https://phucthanhwms.netlify.app"})
 @RequestMapping("/api")
 public class WarehouseController {
 
