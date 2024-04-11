b53b5950b9158ba8d2cbf6fd333954386275540f
TranHieu305 <trunghieu2000.tdn@gmail.com>
2024-04-11 18:36:10 +0700

    Config CORS for development and production

--- src/main/java/com/wms/wms/config/ProdCorsConfig.java
+++ src/main/java/com/wms/wms/config/ProdCorsConfig.java
@@ -1,16 +1,19 @@
 package com.wms.wms.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+@Profile("production")
+public class ProdCorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://phucthanhwms.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
