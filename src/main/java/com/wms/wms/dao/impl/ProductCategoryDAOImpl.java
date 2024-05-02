1e2471e031965900d13cccd9a4a73b2faf8e2d90
TranHieu305 <trunghieu2000.tdn@gmail.com>
2024-05-02 11:51:40 +0700

    Refactor: ProductCategoryDAO

--- src/main/java/com/wms/wms/dao/impl/ProductCategoryDAOImpl.java
+++ src/main/java/com/wms/wms/dao/impl/ProductCategoryDAOImpl.java
@@ -1,5 +1,6 @@
 package com.wms.wms.dao.impl;
 
+import com.wms.wms.dao.AbstractDAO;
 import com.wms.wms.dao.IProductCategoryDAO;
 import com.wms.wms.entity.ProductCategory;
 import jakarta.persistence.EntityManager;
@@ -10,40 +11,37 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class ProductCategoryDAOImpl implements IProductCategoryDAO {
-    private final EntityManager entityManager;
+public class ProductCategoryDAOImpl extends AbstractDAO<ProductCategory> implements IProductCategoryDAO {
+    private EntityManager entityManager;
 
     @Autowired
     public ProductCategoryDAOImpl(EntityManager entityManager) {
-        this.entityManager = entityManager;
+        super(entityManager);
     }
 
     @Override
     public List<ProductCategory> findAll() {
-        TypedQuery<ProductCategory> query = entityManager.createQuery("FROM ProductCategory", ProductCategory.class);
-        return query.getResultList();
+      return super.findAll(ProductCategory.class);
     }
 
     @Override
-    public List<ProductCategory> findByName(String name) {
-        TypedQuery<ProductCategory> query = entityManager.createQuery(
-                "SELECT p FROM ProductCategory p WHERE p.name = :name", ProductCategory.class);
-        query.setParameter("name", name);
-        return query.getResultList();
+    public List<ProductCategory> findByName(String categoryName) {
+        String sql = "SELECT o FROM ProductCategory WHERE o.name = :0";
+        return super.findMany(ProductCategory.class, sql, categoryName);
     }
 
     @Override
-    public ProductCategory findById(int id) {
-        return entityManager.find(ProductCategory.class, id);
+    public ProductCategory findById(int categoryId) {
+       return super.findById(ProductCategory.class, categoryId);
     }
 
     @Override
     public ProductCategory save(ProductCategory productCategory) {
-        return entityManager.merge(productCategory);
+        return super.save(productCategory);
     }
 
     @Override
     public void deleteById(ProductCategory productCategory) {
-        entityManager.remove(productCategory);
+        super.delete(productCategory);
     }
 }
